f0e7abbed65db75d770c21dcbb7b44c13ab274c0
ushapugazh <pugazhenthiusha@gmail.com>
2025-05-23 01:07:11 +0530

    Committing the code before logging off

--- empdetails/src/main/java/com/sb/ms/ang/empdetails/EmpdetailsApplication.java
+++ empdetails/src/main/java/com/sb/ms/ang/empdetails/EmpdetailsApplication.java
@@ -2,8 +2,10 @@ package com.sb.ms.ang.empdetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class EmpdetailsApplication {
 
 	public static void main(String[] args) {
--- empdetails/src/main/java/com/sb/ms/ang/empdetails/controller/EmployeeController.java
+++ empdetails/src/main/java/com/sb/ms/ang/empdetails/controller/EmployeeController.java
@@ -1,10 +1,10 @@
 package com.sb.ms.ang.empdetails.controller;
 
-
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,51 +19,61 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sb.ms.ang.empdetails.model.Employee;
 import com.sb.ms.ang.empdetails.service.ServiceInterface;
 
-
-
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-	
+
 	@Autowired
 	private ServiceInterface empService;
+
 	public EmployeeController() {
-		
-	}
-	
-	public EmployeeController(ServiceInterface empService){
-		this.empService =empService;
+
 	}
 
-    @GetMapping
-    public List<Employee> findAll() {
-        return empService.findAll();
-    }
+//	@Value("${message}")
+	private String welcomeMessage;
 
-    @GetMapping("/{id}")
-    public Optional<Employee> findById(@PathVariable Integer id) {
-        return empService.findById(id);
-    }
+	/*
+	 * @Value("${user.role}") private String userRole;
+	 */
 
-    // create a book
-    @ResponseStatus(HttpStatus.CREATED) // 201
-    @PostMapping
-    public Employee create(@RequestBody Employee emp) {
-        return empService.save(emp);
-    }
+	public EmployeeController(ServiceInterface empService) {
+		this.empService = empService;
+	}
 
-    // update a book
-    @PutMapping
-    public Employee update(@RequestBody Employee emp) {
-        return empService.save(emp);
-    }
+	@GetMapping
+	public List<Employee> findAll() {
+		return empService.findAll();
+	}
 
-    // delete a book
-    @ResponseStatus(HttpStatus.NO_CONTENT) // 204
-    @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Integer id) {
-    	empService.deleteById(id);
-    }
+	@GetMapping("/{id}")
+	public Optional<Employee> findById(@PathVariable Integer id) {
+		return empService.findById(id);
+	}
 
+	// create a book
+	@ResponseStatus(HttpStatus.CREATED) // 201
+	@PostMapping
+	public Employee create(@RequestBody Employee emp) {
+		return empService.save(emp);
+	}
+
+	// update a book
+	@PutMapping
+	public Employee update(@RequestBody Employee emp) {
+		return empService.save(emp);
+	}
+
+	// delete a book
+	@ResponseStatus(HttpStatus.NO_CONTENT) // 204
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable Integer id) {
+		empService.deleteById(id);
+	}
+
+	@GetMapping("/config")
+	public String getConfig() {
+		return String.format( welcomeMessage);
+	}
 
 }
